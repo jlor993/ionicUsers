a096a20abeab286d4f33b634f64b7f466c138b1a
Jeffrey Lor <jlor993@gmail.com>
2021-10-25 10:19:24 -0500

    Register and base for logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { RouteReuseStrategy } from '@angular/router';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/logout/logout.page.ts
+++ src/app/logout/logout.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutPage implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter(){
+    this.logout();
+  }
+
+  logout(): void {
+    this.authService.logout().subscribe(
+      (response:any) => {
+        if(response.success == 'true')  {
+          this.router.navigate(['/login']);
+        }
+      }
+    );
   }
 
 }
